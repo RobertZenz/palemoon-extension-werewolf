bf106e80f0aaac655c0906b30b7016df6da91a32
Robert Zenz <Robert.Zenz@bonsaimind.org>
2018-01-13 13:56:23 +0100

    Fixed selector.

--- src/content/Werewolf.js
+++ src/content/Werewolf.js
@@ -79,13 +79,13 @@ var Werewolf = {
 		
 		this.preferences.registerInt("bookmarks.bookmark-folder.padding-left", function(name, value) {
 			this.styleSheet.register(name, new CSSBuilder()
-					.addSelector("#PlacesToolbarItems > .bookmark-item[type=menu]:first-child")
+					.addSelector("#PlacesToolbarItems > .bookmark-item[type=menu] > :first-child")
 					.autoPadding("left", value));
 		}.bind(this));
 		
 		this.preferences.registerInt("bookmarks.bookmark-folder.padding-right", function(name, value) {
 			this.styleSheet.register(name, new CSSBuilder()
-					.addSelector("#PlacesToolbarItems > .bookmark-item[type=menu]:last-child")
+					.addSelector("#PlacesToolbarItems > .bookmark-item[type=menu] > :last-child")
 					.autoPadding("right", value));
 		}.bind(this));
 		
@@ -123,13 +123,13 @@ var Werewolf = {
 		
 		this.preferences.registerInt("bookmarks.bookmark-item.padding-left", function(name, value) {
 			this.styleSheet.register(name, new CSSBuilder()
-					.addSelector("#PlacesToolbarItems > .bookmark-item:not([type]):first-child")
+					.addSelector("#PlacesToolbarItems > .bookmark-item:not([type]) > :first-child")
 					.autoPadding("left", value));
 		}.bind(this));
 		
 		this.preferences.registerInt("bookmarks.bookmark-item.padding-right", function(name, value) {
 			this.styleSheet.register(name, new CSSBuilder()
-					.addSelector("#PlacesToolbarItems > .bookmark-item:not([type]):last-child")
+					.addSelector("#PlacesToolbarItems > .bookmark-item:not([type]) > :last-child")
 					.autoPadding("right", value));
 		}.bind(this));
 		
